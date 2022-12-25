e7d77a467b0deb8ffd031fbd8d9c79a5112c0328
felixingla <79790608+felixingla@users.noreply.github.com>
2022-12-25 19:20:29 +0100

    Forms and Events

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,20 +2,27 @@ import React from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import { TasksCollection } from '/imports/api/TasksCollection';
 import { Task } from './Task';
+import { TaskForm } from './TaskForm';
 
 /*Displays a list of tasks fetched from a Meteor collection called TasksCollection*/
 export const App = () => {
   /*Fetch tasks from collection*/
-  const tasks = useTracker(() => TasksCollection.find({}).fetch());
+  const tasks = useTracker(() => 
+    TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch()
+  );
 
   /*Return the fetched tasks from the collection*/
   return (
     <div>
       <h1>Welcome to Meteor!</h1>
       
+      <TaskForm />
+
       <ul>
         {/* Iterate over the tasks constant to render the task text as a list */}
-        { tasks.map(task => <Task key={ task._id } task={ task }/>) }
+        {tasks.map(task => (
+          <Task key={task._id} task={task} />
+        ))}
       </ul>
     </div>
   );
---
+++ imports/ui/TaskForm.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { TasksCollection } from '/imports/api/TasksCollection';
+
+// Define the TaskForm component
+export const TaskForm = () => {
+  // Declare a state variable called "text" and a function called "setText" for updating it
+  // Initially, the "text" variable is set to an empty string
+  const [text, setText] = useState('');
+
+  // Define the handleSubmit event handler function
+  const handleSubmit = e => {
+    // Prevent the default refresh behavior of the form submission
+    e.preventDefault();
+
+    // If the "text" variable is empty, do nothing
+    if (!text) return;
+
+    // Insert a new task object into the TasksCollection with the trimmed value of "text" and the current date
+    TasksCollection.insert({
+      text: text.trim(),
+      createdAt: new Date(),
+    });
+
+    // Set the value of "text" back to an empty string
+    setText('');
+  };
+
+  // Return the JSX for the TaskForm component
+  return (
+    
+    <form className="task-form" onSubmit={handleSubmit}>
+    
+      <input
+        type="text"
+        placeholder="Type to add new tasks"
+        value={text}
+        onChange={e => setText(e.target.value)}
+      />
+
+      <button type="submit">Add Task</button>
+    </form>
+  );
+};
