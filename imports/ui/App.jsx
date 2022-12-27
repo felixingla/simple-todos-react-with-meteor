8c094a65219adb2a4901104d99d8c3ccbb69997c
felixingla <79790608+felixingla@users.noreply.github.com>
2022-12-27 21:02:07 +0100

    Filter tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import { TasksCollection } from '/imports/api/TasksCollection';
 import { Task } from './Task';
@@ -16,38 +16,65 @@ const toggleChecked = ({ _id, isChecked}) => {
 // Function to delete a task
 const deleteTask = ({_id}) => TasksCollection.remove(_id);
 
-/*Displays a list of tasks fetched from a Meteor collection called TasksCollection*/
+// Displays a list of tasks fetched from a Meteor collection called TasksCollection
 export const App = () => {
-  /*Fetch tasks from collection*/
-  const tasks = useTracker(() => 
-    TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch()
+
+  // Declare state function to filter tasks
+  const [hideCompleted, setHideCompleted] = useState(false);
+
+  // Declare state function to hide completed tasks
+  const hideCompletedFilter = { isChecked: { $ne: true}};
+
+  // Declare state function to show pending tasks
+  const pendingTasksCount = useTracker(() => 
+    TasksCollection.find(hideCompletedFilter).count()
   );
 
-  /*Return the fetched tasks from the collection*/
+  // ???
+  const pendingTasksTitle = `${
+    pendingTasksCount ? ` (${pendingTasksCount})` : ''
+  }`;
+  
+  // Fetch tasks from collection
+  const tasks = useTracker(() => 
+    TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, { sort: { createdAt: -1 } }).fetch()
+  );
+
+  // Return the fetched tasks from the collection, text, form, and buttons
   return (
     <div className='App'>
       <header>
         <div className='app-bar'>
           <div className='app-headr'>
             <h1>To do list </h1>
+            <h6> You have {pendingTasksTitle} pending tasks </h6>            
             </div>
         </div>
       </header>
 
       <div className='main'>
+      
+      {/* Return TaskForm component */}
       <TaskForm />
 
-        <ul className='tasks'>
-          {/* Iterate over the tasks constant to render the task text as a list */}
-          {tasks.map(task => (
-            <Task 
-              key={task._id} 
-              task={task} 
-              onChecboxClick={toggleChecked} 
-              onDeleteClick={deleteTask}
-            />
-          ))}
-        </ul>
+      {/* Button for filtering tasks */}
+      <div className='filter'>
+        <button onClick={() => setHideCompleted(!hideCompleted)}>
+          {hideCompleted ? 'Show all' : 'Show pending tasks'}
+        </button>
+      </div>
+
+      <ul className='tasks'>
+        {/* Iterate over the tasks constant to render the task text as a list */}
+        {tasks.map(task => (
+          <Task 
+            key={task._id} 
+            task={task} 
+            onChecboxClick={toggleChecked} 
+            onDeleteClick={deleteTask}
+          />
+        ))}
+      </ul>
       </div>
     </div>
   );
